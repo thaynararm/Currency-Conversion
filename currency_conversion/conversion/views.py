541de75dca95944e222eeea72e930fd6627fbedc
Thay <thaynararm2@hotmail.com>
2024-02-29 16:59:02 -0300

    FEAT: Inserção do método post

--- currency_conversion/conversion/views.py
+++ currency_conversion/conversion/views.py
@@ -13,7 +13,6 @@ def get_data_API() -> dict:
 
     try:
         response = requests.get(url).json()
-        response.raise_for_status()
         quotes = response.items()
     
     except requests.exceptions.RequestException as e:
@@ -21,6 +20,29 @@ def get_data_API() -> dict:
 
     return quotes
 
+
+def perform_currency_conversion(from_currency, to_currency, amount) -> dict:
+
+    # Obtém as taxas de câmbio
+    price = dict(get_data_API())
+
+    #Realiza a conversão de moeda
+    if from_currency != to_currency:
+        
+        usd = amount/price['rates'][from_currency]
+        value = usd * price['rates'][to_currency]
+
+        # Retorna o resultado convertido
+        response_data = {
+            'from_currency': from_currency,
+            'to_currency': to_currency,
+            'amount': amount,
+            'converted_amount': value
+        }
+        
+        return response_data
+        
+    return Response({'error': 'A moeda de origem deve ser diferente da moeda final'}, status=status.HTTP_400_BAD_REQUEST)
 
 class CurrencyConverterView(APIView):
 
@@ -48,28 +70,30 @@ class CurrencyConverterView(APIView):
         if not from_currency or not to_currency or amount <= 0:
             return Response({'error': 'A requisição deve receber moeda de origem, moeda final e um valor válido.'}, status=status.HTTP_400_BAD_REQUEST)
         
-        # Obtém as taxas de câmbio
-        price = dict(get_data_API())
+        response_data = perform_currency_conversion(from_currency, to_currency, amount)
 
-        #Realiza a conversão de moeda
-        if from_currency != to_currency:
-            
-            usd = amount/price['rates'][from_currency]
-            value = usd * price['rates'][to_currency]
-
-            # Retorna o resultado convertido
-            response_data = {
-                'from_currency': from_currency,
-                'to_currency': to_currency,
-                'amount': amount,
-                'converted_amount': value
-            }
-            
-            return Response(response_data)
-            
-        return Response({'error': 'A moeda de origem deve ser diferente da moeda final'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(response_data)
+    
 
 
+    def post(self, request):
 
+        #Extração dos parâmetros da URL de requisição
+        from_currency = request.GET.get('from', '').upper()
+        to_currency = request.GET.get('to', '').upper()
+        amount = float(request.GET.get('amount', 0))
+        
+        # Validação da entrada do valor a ser convertido para float
+        try:
+            amount = float(amount)
+        except ValueError:
+            return Response({'error': 'O valor a ser convertido deve ser um número válido. Utilize o ponto como separador de números decimais'}, status=status.HTTP_400_BAD_REQUEST)
+        
 
+        # Validação da entrada dos parâmetros: moeda de origem, valor a ser convertido e moeda final
+        if not from_currency or not to_currency or amount <= 0:
+            return Response({'error': 'A requisição deve receber moeda de origem, moeda final e um valor válido.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        response_data = perform_currency_conversion(from_currency, to_currency, amount)
 
+        return Response(response_data)
